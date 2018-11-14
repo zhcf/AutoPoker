f2da2cb8d04da0753d8098e2b087524cb184cb56
Minhua MH1 Weng <wengmh1@lenovo.com>
2018-11-14 16:15:37 +0800

    Fix calculator test

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -4,6 +4,6 @@ from pokers.calculator import Calculator
 def test_get_hand_strength():
     calculator = Calculator()
     hand_cards = utils.parse_cards(['TS', 'JS'])
-    river_cards = utils.parse_cards(['9C', 'KS', '2D', '3D', '4S'])
+    river_cards = utils.parse_cards(['QS', 'KS', 'AS', '3D', '4H'])
     strength = calculator.get_hand_strength(hand_cards, river_cards, 5, 1000)
     assert strength == 1
