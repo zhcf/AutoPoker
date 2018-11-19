ca12c48ab2f858e50bb141d8c4a84924d18e4f2a
Minhua MH1 Weng <wengmh1@lenovo.com>
2018-11-19 18:17:45 +0800

    Init best pratice

---
+++ best_practice/hand_strength_constriction.py
@@ -0,0 +1,15 @@
+from pokers/calculator import Calculator
+
+ROYAL_FLUSH = ['TS', 'JS', 'QS', 'KS', 'AS']
+STRAIGHT_FLUSH = ['5C', '6C', '7C', '8C', '9C']
+FOUR_OF_A_KIND = ['8C', 'AD', 'AC', 'AS', 'AH']
+FULL_HOUSE = ['KC', 'KH', 'KS', 'TC', 'TS']
+FLUSH = ['QC', '8C', '6C', '4C', '3C']
+STRAIGHT = ['2D', '3C', '4S', '5D', '6S']
+THREE_OF_A_KIND = ['JD', 'JC', 'JH', '5C', '8D']
+HT_TWO_PAIR = ['6S', '6C', 'TC', 'TD', 'KD']
+ONE_PAIR = ['2D', '6H', '8S', 'AC', 'AH']
+HIGH_CARD = ['KH', 'JS', 'TC', '6H','3D']
+
+if __name__ == 'main':
+    
